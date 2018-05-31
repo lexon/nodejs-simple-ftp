3e45dde50a3597913d55bf4345e96fe871e39b0d
Alexander Bauer <alex@abmbp.fritz.box>
2018-05-31 17:40:37 +0200

    removed dist

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("./src/server");
-const config = new server_1.ServerConfig('127.0.0.1', 1337, '/Users/alex/tmp/fttp');
-let ftp = new server_1.Server(config);
-ftp.start();
-//# sourceMappingURL=index.js.map
--- dist/src/commands/PasvCommand.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Command_1 = require("./Command");
-class PasvCommand extends Command_1.default {
-}
-exports.default = PasvCommand;
-//# sourceMappingURL=PasvCommand.js.map
